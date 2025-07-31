106b16bcca735e356c8c1b7b8f9f7258b991daf7
Jerwin Peralta <jerwin.peralta@infor.com>
2025-07-31 14:28:44 +0800

    formatting

--- acs-setup/modules/check-dotnet.ps1
+++ acs-setup/modules/check-dotnet.ps1
@@ -5,11 +5,13 @@ if ($dotnet) {
     $version = (& $dotnet --version)
     if ($version -like "8.*") {
         Write-Log ".NET 8 is installed: version $version"
-    } else {
+    }
+    else {
         Write-Log ".NET found, but not version 8+: $version"
         $env:ACS_Install_DotNet = $true
     }
-} else {
+}
+else {
     Write-Log ".NET not found."
     $env:ACS_Install_DotNet = $true
 }
